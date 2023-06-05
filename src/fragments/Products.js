49827276eb3b613e4d7bd53decbd435a313e1ed3
aiwebsite <134672407+aiwebsite@users.noreply.github.com>
2023-06-05 20:28:23 +0800

    Initialize TITAN-153

---
+++ src/fragments/Products.js
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export const ProductsFragment = gql`
+  fragment ProductsFragment on Product {
+    id
+    name
+    description
+    price
+    salePrice
+    reviewsRating
+    totalSold
+    bigCommerceID
+    slug
+    productFormFieldsJson
+    relatedProducts
+    variantLookupJson
+    modifierLookupJson
+    images {
+      edges {
+        node {
+          id
+          description
+          urlStandard
+          urlZoom
+        }
+      }
+    }
+  }
+`;
+
+export const ProductFragment = gql`
+  fragment ProductFragment on Product {
+    id
+    name
+    sku
+    description
+    price
+    salePrice
+    calculatedPrice
+    reviewsRating
+    totalSold
+    bigCommerceID
+    slug
+    productFormFieldsJson
+    relatedProducts
+    variantLookupJson
+    modifierLookupJson
+    variants(last: 1) {
+      nodes {
+        bigCommerceVariantID
+      }
+    }
+    images {
+      edges {
+        node {
+          id
+          description
+          urlStandard
+          urlZoom
+          urlThumbnail
+        }
+      }
+    }
+  }
+`;
