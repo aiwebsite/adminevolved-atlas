a23e81b585176a0c0a85f4db4c8cb73fa75e24eb
aiwebsite <134672407+aiwebsite@users.noreply.github.com>
2023-06-05 20:28:23 +0800

    Initialize RossoMaguire/update-acm-json

---
+++ src/components/ContentWrapper/ContentWrapper.js
@@ -0,0 +1,14 @@
+import className from 'classnames/bind';
+import styles from './ContentWrapper.new.module.scss';
+
+let cx = className.bind(styles);
+
+export default function ContentWrapper({ content, children }) {
+  console.log(styles);
+  return (
+    <article className={cx(styles.content, 'component')}>
+      <div dangerouslySetInnerHTML={{ __html: content ?? '' }} />
+      {children}
+    </article>
+  );
+}
